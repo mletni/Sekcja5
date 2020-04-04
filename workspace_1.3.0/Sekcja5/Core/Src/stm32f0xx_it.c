e1c725c068a09f89d5a38723093e1f2d18e79697
KroneckerCapelli <m.letni@gmail.com>
2020-04-04 21:58:50 +0200

    message

--- workspace_1.3.0/Sekcja5/Core/Src/stm32f0xx_it.c
+++ workspace_1.3.0/Sekcja5/Core/Src/stm32f0xx_it.c
@@ -139,11 +139,11 @@ void PendSV_Handler(void)
 void SysTick_Handler(void)
 {
   /* USER CODE BEGIN SysTick_IRQn 0 */
+    TimingDelay_Decrement();
 
   /* USER CODE END SysTick_IRQn 0 */
   HAL_IncTick();
   /* USER CODE BEGIN SysTick_IRQn 1 */
-    TimingDelay_Decrement();
   /* USER CODE END SysTick_IRQn 1 */
 }
 
